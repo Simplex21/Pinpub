8395ce548fb10ff63773ed20b39c72ef690ad87a
Simplex21 <21simplex02@gmail.com>
2019-02-17 22:24:28 +0100

    Bildtauscher, ImageButtons

--- app/src/main/java/com/simonus/pinpub/MultiplicationActivity.java
+++ app/src/main/java/com/simonus/pinpub/MultiplicationActivity.java
@@ -1,13 +1,25 @@
 package com.simonus.pinpub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 
 public class MultiplicationActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplication);
+
+        ImageButton imgBtnCam = findViewById(R.id.imageCamBtn);
+        imgBtnCam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Camera betätigt", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/simonus/pinpub/SubtractionActivity.java
+++ app/src/main/java/com/simonus/pinpub/SubtractionActivity.java
@@ -1,13 +1,17 @@
 package com.simonus.pinpub;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import javax.security.auth.Destroyable;
+
 public class SubtractionActivity extends AppCompatActivity {
 
+    public int currentImageId = R.drawable.bali;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +23,23 @@ public class SubtractionActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 ImageView simpleImg = findViewById(R.id.simpleImage);
-                boolean imgSwitch = false;
 
-                //if (simpleImg.getDrawable() == R.drawable.bali) {
-                //    // do something
-                //}
+                if (currentImageId == R.drawable.bali) {
+                    currentImageId = R.drawable.lighthouse;
+                } else {
+                    currentImageId = R.drawable.bali;
+                }
+
+                simpleImg.setImageResource(currentImageId);
+            }
+        });
+
+        simpleBtn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ImageView simpleImg = findViewById(R.id.simpleImage);
+                simpleImg.setImageResource(R.drawable.killer);
+                return true;
             }
         });
     }
